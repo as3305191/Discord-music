a720187075ef7f6adaf93bd6aee97b8922d23f4c
江官駿 <as3305191@gmail.com>
2020-06-27 06:25:36 +0800

    update

--- index.js
+++ index.js
@@ -3,8 +3,53 @@ const { prefix, token } = require("./config.json");
 const ytdl = require("ytdl-core");
 
 const client = new Discord.Client();
-
 const queue = new Map();
+
+var http = require('http');
+ var fs = require('fs');
+ var path = require('path');
+
+ http.createServer(function (request, response) {
+
+    console.log('request starting for ');
+    console.log(request);
+
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
+
+    console.log(filePath);
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+    path.exists(filePath, function(exists) {
+
+      if (exists) {
+          fs.readFile(filePath, function(error, content) {
+              if (error) {
+                  response.writeHead(500);
+                  response.end();
+              }
+              else {
+                  response.writeHead(200, { 'Content-Type': contentType });
+                  response.end(content, 'utf-8');
+              }
+          });
+      }
+      else {
+          response.writeHead(404);
+          response.end();
+      }
+  });
+
+}).listen(process.env.PORT || 5000);
 
 client.once("ready", () => {
   console.log("機器人已經準備就緒!");
